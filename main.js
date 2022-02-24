465090c3e025a4a7bee575271ddcb77b5eb9cb05
Usuario <usis011120@ugb.edu.sv>
2022-02-23 21:56:41 -0600

    practica

--- main.js
+++ main.js
@@ -1,104 +1,46 @@
-var db_sistema = openDatabase('dbsistema', '1.0', 'Sistema de Facturacion', 5 * 1024 * 1024);
-if(!db_sistema){
-    alert('Lo siento tu navegado NO soporta BD locales.');
-}
-var app = new Vue({
-    el: '#appCliente',
-    data: {
-        clientes: [],
-        buscar: '',
-        cliente: {
-            accion: 'nuevo',
-            msg : '',
-            idCliente: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            dui: ''
-        },
+var generarIdUnicoDesdeFecha=()=>{
+    let fecha = new Date();
+    return Math.floor(fecha.getTime()/1000).toString(16);
+}, db;
+var appVue = new Vue({
+    el:'#appSistema',
+    data:{
+        forms:{
+            'lectura':{mostrar:false},
+            'cliente':{mostrar:false},
+        }
     },
-    methods: {
-        buscarCliente(){
-            /*if( this.buscar.trim().length>0 ){
-                this.clientes = this.clientes.filter(item=>item.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>=0);
-            } else {
-                this.obtenerClientes();
-            }*/
-            this.obtenerClientes(this.buscar);
-        },
-        guardarCliente(){
-            let sql = '',
-                parametros = [];
-            if(this.cliente.accion == 'nuevo'){
-                sql = 'INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)';
-                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui];
-            }else if(this.cliente.accion == 'modificar'){
-                sql = 'UPDATE clientes SET codigo=?, nombre=?, direccion=?, telefono=?, dui=? WHERE idCliente=?';
-                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui,this.cliente.idCliente];
-            }else if(this.cliente.accion == 'eliminar'){
-                sql = 'DELETE FROM clientes WHERE idCliente=?';
-                parametros = [this.cliente.idCliente];
-            }
-            db_sistema.transaction(tx=>{
-                tx.executeSql(sql,
-                    parametros,
-                (tx, results)=>{
-                    this.cliente.msg = 'Cliente procesado con exito';
-                    this.nuevoCliente();
-                    this.obtenerClientes();
-                },
-                (tx, error)=>{
-                    switch(error.code){
-                        case 6:
-                            this.cliente.msg ='El codigo o el Dui ya existe,por favor digite otro';
-                            break;
-                            default:
-                                this.cliente.msg ='error al procesar el cliente: ${error.message}';
-                    }
-                    this.cliente.msg = `Error al guardar el cliente ${error.message}`;
-                });
-            });
-        },
-        modificarCliente(cliente){
-            this.cliente = cliente;
-            this.cliente.accion = 'modificar';
-        },
-        eliminarCliente(cliente){
-            if( confirm(`¿Esta seguro de eliminar el cliente ${cliente.nombre}?`) ){
-                this.cliente.idCliente = cliente.idCliente;
-                this.cliente.accion = 'eliminar';
-                this.guardarCliente();
-            }
-        },
-        obtenerClientes(busqueda=''){
-            db_sistema.transaction(tx=>{
-                tx.executeSql(`SELECT * FROM clientes WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
-                    this.clientes = results.rows;
-                    /*this.clientes = [];
-                    for(let i=0; i<results.rows.length; i++){
-                        this.clientes.push(results.rows.item(i));
-                    }*/
-                });
-            });
-        },
-        nuevoCliente(){
-            this.cliente.accion = 'nuevo';
-            this.cliente.idCliente = '';
-            this.cliente.codigo = '';
-            this.cliente.nombre = '';
-            this.cliente.direccion = '';
-            this.cliente.telefono = '';
-            this.cliente.dui = '';
+    methods:{
+        abrirBd(){
+            let indexDb = indexedDB.open('db_sistema_agua_potable',1);
+            indexDb.onupgradeneeded=event=>{
+                let req=event.target.result,
+                    tbllecturas = req.createObjectStore('tbllecturas',{keyPath:'idLectura'}),
+                    tblclientes = req.createObjectStore('tblclientes',{keyPath:'idCliente'});
+                
+                tbllecturas.createIndex('idLectura','idLectura',{unique:true});
+                
+                tblclientes.createIndex('idCliente','idCliente',{unique:true});
+                tblclientes.createIndex('codigo','codigo',{unique:false});
+            };
+            indexDb.onsuccess = evt=>{
+                db=evt.target.result;
+            };
+            indexDb.onerror=e=>{
+                console.log("Error al conectar a la BD", e);
+            };
         }
     },
     created(){
-        db_sistema.transaction(tx=>{
-            tx.executeSql('CREATE TABLE IF NOT EXISTS clientes(idCliente INTEGER PRIMARY KEY AUTOINCREMENT, '+
-                'codigo char(10) unique, nombre char(75), direccion TEXT, telefono char(10), dui char(10) unique)');
-        }, err=>{
-            console.log('Error al crear la tabla de clientes', err);
-        });
-        this.obtenerClientes();
+        this.abrirBd();
     }
+});
+
+document.addEventListener("DOMContentLoaded",event=>{
+    let el = document.querySelectorAll(".mostrar").forEach( (element, index)=>{
+        element.addEventListener("click",evt=>{
+            appVue.forms[evt.target.dataset.form].mostrar = true;
+            appVue.$refs[evt.target.dataset.form].obtenerDatos();
+        });
+    } );
 });
